b80480f2fe0bf696780252c9cb18effd269f331f
Hussein Dawod <56936130+hdtv0199@users.noreply.github.com>
2023-06-04 03:55:53 -0400

    Completed Responsive Navbar in Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,43 +1,83 @@
 import React, { useState } from 'react'
-import {NavLink, Outlet} from 'react-router-dom'
-import '../styles.css'
+import { NavLink, Outlet } from 'react-router-dom'
+import Hamburger from 'hamburger-react'
 import Footer from './Footer'
-import {FiMenu} from 'react-icons/fi'
 
 const Layout: React.FC = () => {
+  const [open, setOpen] = useState<boolean>(false)
 
-    // className='nav absolute md:static w-auto h-auto top-0 custom:flex custom:flex-row flex flex-col items-center justify-evenly'
+  const toggleMenu = () => setOpen(!open)
 
-    // className='nav absolute custom:static w-auto h-auto top-0 custom:flex custom:flex-row flex flex-col items-center justify-evenly'
+  return (
+    <div>
+      <div className="flex items-center justify-between lg:mx-auto p-7">
+        <NavLink to="/" className="text-white font-bold text-3xl">
+          GPT-4
+        </NavLink>
 
-    const [open, setOpen] = useState(false)
-
-    return (
-
-        <div>
-                <div className='flex items-center justify-between lg:mx-auto lg:max-w-7xl max-w-full px-[8%] w-full flex-wrap p-8'>
-                    <NavLink to='/' className='text-white font-bold text-3xl'>GPT-4</NavLink>
-
-                    <FiMenu className="lg:hidden block h-6 w-6 cursor-pointer" color='white' onClick={() => setOpen(!open)} />
-
-                    <div className={`${open ? "flex items-center" : "hidden"} lg:flex lg:items-center lg:w-auto w-full`}>
-                        <div className=' nav-items text-sm lg:text-base text-white lg:mx-3'>
-                            <NavLink to='/' >Home</NavLink>
-                            <NavLink to='/about' >What is GPT?</NavLink>
-                            <NavLink to='/open-ai' >Open AI</NavLink>
-                            <NavLink to='/case-studies' >Case Studies</NavLink>
-                            <NavLink to='/library' >Library</NavLink>
-                        </div>
-                        <div>
-                            <NavLink to='/login' className='text-white text-lg mx-4'>Log In</NavLink> 
-                            <NavLink to='/signup' className='text-white text-lg bg-red-500 lg:w-[170px] lg:h-[58px] h-[20px] w-[80] p-3 rounded-md'>Sign Up</NavLink>
-                        </div>
-                    </div>
-                </div>
-            <Outlet />
-            <Footer />
+        <div
+          className={`${
+            open ? 'flex justify-end' : 'hidden'
+          } lg:flex lg:items-center lg:w-auto w-full`}
+        >
+          <div className="nav-items text-sm lg:text-base text-white lg:mx-3">
+            <NavLink
+              to="/"
+              className="block lg:inline-block text-white lg:hover:text-gray-500 px-4 py-2"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/about"
+              className="block lg:inline-block text-white lg:hover:text-gray-500 px-4 py-2"
+            >
+              What is GPT?
+            </NavLink>
+            <NavLink
+              to="/open-ai"
+              className="block lg:inline-block text-white lg:hover:text-gray-500 px-4 py-2"
+            >
+              Open AI
+            </NavLink>
+            <NavLink
+              to="/case-studies"
+              className="block lg:inline-block text-white lg:hover:text-gray-500 px-4 py-2"
+            >
+              Case Studies
+            </NavLink>
+            <NavLink
+              to="/library"
+              className="block lg:inline-block text-white lg:hover:text-gray-500 px-4 py-2"
+            >
+              Library
+            </NavLink>
+          </div>
+          <div className="flex flex-col lg:block">
+            <NavLink
+              to="/login"
+              className="text-white text-sm lg:text-lg mx-4"
+            >
+              Log In
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className="text-white text-sm my-2 lg:text-lg lg:my-0 bg-red-500 lg:w-[170px] lg:h-[58px] p-2 rounded-md"
+            >
+              Sign Up
+            </NavLink>
+          </div>
         </div>
-    )
-}
 
-export default Layout
+        <div className="lg:hidden mx-4 h-6 w-6 cursor-pointer" onClick={toggleMenu}>
+            <Hamburger color="white" />
+        </div>
+      </div>
+
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
+
